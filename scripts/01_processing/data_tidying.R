7fc705e70f1d13d14f9406a6f12027bd985a8e18
aeob224 <aidan.obrien823@gmail.com>
2025-10-18 15:48:02 -0400

    Completed code for summarizing morphometric data into average body condition by year and pond.

--- scripts/01_processing/data_tidying.R
+++ scripts/01_processing/data_tidying.R
@@ -24,12 +24,21 @@ aquatic_data <- read_csv("data/raw/aquatic_variables.csv") |>
   unite(col = "pond_year",
         pond, year)
 
-# PROCESSING ###################################################################
+# PROCESSING morphometric data into mean body condition by pond and year###################################################################
 ## Filter morphometric data entries missing SVL and mass and log transforms them
 filtered_morphometric <- morphometric_data |>
   filter_at(vars(mass, svl), all_vars(!is.na(.))) |>
   mutate(svl = log(svl)) |>
-  mutate(mass = log(mass))
+  mutate(mass = log(mass)) |>
+
+##Adding a column for body condition
+##Note: Body condition is calculated as the residuals of log(mass) vs. log(snout to vent length) in amphibians
+filtered_morphometric$body_condition <- residuals(lm(mass ~ svl, filtered_morphometric))
+
+##Summarizing body condition by pond and year
+summary_body_condition <- filtered_morphometric |>
+  group_by(pond_year) |>
+  summarize(body_condition = mean(body_condition))
 
 
 
@@ -38,5 +47,3 @@ filtered_morphometric <- morphometric_data |>
 body_conditions <-
 
 
-
-?mutate
