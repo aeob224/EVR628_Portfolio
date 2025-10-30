632f57cfa35a4e9e9629ca4ab43dbdd4b2531b90
aeob224 <aidan.obrien823@gmail.com>
2025-10-30 08:56:17 -0400

    Added some exploratory analysis

--- scripts/03_content/data_visualization.R
+++ scripts/03_content/data_visualization.R
@@ -13,3 +13,13 @@ library(tidyverse)
 library(EVR628tools)
 
 # Load Data ####################################################################
+data <- read_csv("data/processed/tidy_data.csv")
+
+#Initial Analysis ##############################################################
+
+
+
+
+#Plot Density against
+plot(x = data$larv_density,
+     y = data$body_condition)
