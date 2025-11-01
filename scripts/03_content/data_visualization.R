5566e88548c15284c65fe1d731e2c5784b743a7a
aeob224 <aidan.obrien823@gmail.com>
2025-11-01 13:28:55 -0400

    Added script for comparing body condition to prey abundance

--- scripts/03_content/data_visualization.R
+++ scripts/03_content/data_visualization.R
@@ -15,12 +15,9 @@ library(EVR628tools)
 # Load Data ####################################################################
 data <- read_csv("data/processed/tidy_data.csv")
 
-#Initial Analysis ##############################################################
 
-
-
-
-#Plot Density against body condition
+#Plot Density against body condition ###########################################
+##Note: Both measures are unitless as presented.
 ggplot(data = data,
        mapping = aes(x = log(larv_density),
                      y = body_condition))+
@@ -31,3 +28,39 @@ ggplot(data = data,
        subtitle = "Relating Santa Cruz Long-toed salamander larval abundance to health",
        x = "log(Larval Density)",
        y = "Mean Body Condition")
+
+
+#Plot Body condition against multiple stressors and create a multi-plot
+##Salinity plot
+ggplot(data = data,
+       mapping = aes(x = log(salinity +0.1),
+                     y = body_condition))+
+  geom_smooth(method = "lm",
+              color = "black")+
+  geom_point(color = "steelblue")+
+  labs(x = "log(Salinity)",
+       y = "Mean Body Condition")
+
+
+
+#Body Condition against plankton abundance
+ggplot(data = data,
+       mapping = aes(x = log(plankton +1),
+                     y = body_condition))+
+  geom_smooth(method = "lm",
+              color = "black")+
+  geom_point(color = "steelblue")+
+  labs(x = "log(# of Plankton)",
+       y = "Mean Body Condition")
+
+#Body Condition against medium prey abundance
+ggplot(data = data,
+       mapping = aes(x = log(med_prey +1),
+                     y = body_condition))+
+  geom_smooth(method = "lm",
+              color = "black")+
+  geom_point(color = "steelblue")+
+  labs(x = "log(Medium Prey Abundance)",
+       y = "Mean Body Condition")
+
+#
