4667b458c63e3d14856824666b2dfa30150b6c51
aeob224 <aidan.obrien823@gmail.com>
2025-11-01 12:33:25 -0400

    Added script for relating and visualizing SCLTS body condition to density

--- scripts/03_content/data_visualization.R
+++ scripts/03_content/data_visualization.R
@@ -20,6 +20,14 @@ data <- read_csv("data/processed/tidy_data.csv")
 
 
 
-#Plot Density against
-plot(x = data$larv_density,
-     y = data$body_condition)
+#Plot Density against body condition
+ggplot(data = data,
+       mapping = aes(x = log(larv_density),
+                     y = body_condition))+
+  geom_smooth(method = "gam",
+              color = "black")+
+  geom_point(color = "steelblue")+
+  labs(title = "Generalized Additive Model of Body Condition vs. Density",
+       subtitle = "Relating Santa Cruz Long-toed salamander larval abundance to health",
+       x = "log(Larval Density)",
+       y = "Mean Body Condition")
